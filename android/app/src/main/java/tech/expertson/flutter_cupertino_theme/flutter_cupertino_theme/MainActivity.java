9de6dda1ef038ad29eebf9c4b3dd9911a8f30e45
Faiz Muttaqin <38288096+faiz-muttaqin@users.noreply.github.com>
2024-01-02 09:04:43 +0700

    Initial Commit Cupertino Theme 1 page

---
+++ android/app/src/main/java/tech/expertson/flutter_cupertino_theme/flutter_cupertino_theme/MainActivity.java
@@ -0,0 +1,6 @@
+package tech.expertson.flutter_cupertino_theme.flutter_cupertino_theme;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
